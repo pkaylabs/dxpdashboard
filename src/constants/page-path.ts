b591c6dc892f09219e4c1ac79a91d6fa500869f4
princek <princesamuelpks@gmail>
2025-07-05 14:03:51 +0000

    replaced the api url

--- src/constants/page-path.ts
+++ src/constants/page-path.ts
@@ -1,2 +1,4 @@
 
-export const BACKEND_BASE_URL = "http://143.198.237.184/api-v1/";
+// export const BACKEND_BASE_URL = "http://143.198.237.184/api-v1/";
+export const BACKEND_BASE_URL = "https://api.bayelsaxp.com/api-v1/";
+
